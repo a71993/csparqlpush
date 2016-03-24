9ea6d5dc3c162796f9d5a766c6ccc3fa0f6aad9e
a71993 <a71993@ut.ee>
2016-03-24 08:10:08 +0200

    small fixes

--- client-receiver/src/main/java/hubclient/resources/ListOfRDFTriples.java
+++ client-receiver/src/main/java/hubclient/resources/ListOfRDFTriples.java
@@ -5,9 +5,5 @@ import java.util.ArrayList;
 public class ListOfRDFTriples {
 	
 	public static ArrayList<RDFTriple> listOfRDFTriples;
-	
-	public ListOfRDFTriples() {
-		super();
-	}
 
 }
--- rsp_services_csparql/src/main/java/polimi/deib/rsp_services_csparql/streams/SingleStreamDataServer.java
+++ rsp_services_csparql/src/main/java/polimi/deib/rsp_services_csparql/streams/SingleStreamDataServer.java
@@ -20,27 +20,16 @@
  ******************************************************************************/
 package polimi.deib.rsp_services_csparql.streams;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.List;
-
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.restlet.data.ClientInfo;
 import org.restlet.data.MediaType;
@@ -62,18 +51,12 @@ import polimi.deib.rsp_services_csparql.commons.Csparql_Engine;
 import polimi.deib.rsp_services_csparql.commons.Csparql_RDF_Stream;
 import polimi.deib.rsp_services_csparql.commons.Utilities;
 import polimi.deib.rsp_services_csparql.streams.utilities.CsparqlStreamDescriptionForGet;
-import polimi.deib.rsp_services_csparql.server.rsp_services_csparql_server;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-
 import eu.larkc.csparql.cep.api.RdfQuadruple;
 import eu.larkc.csparql.cep.api.RdfStream;
 
